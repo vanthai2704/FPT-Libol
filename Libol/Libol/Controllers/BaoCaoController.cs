b1d9e07dca6948e2335e45b6bb747a3b29750077
Nguyen Xuan Tan An <nxtanan@hotmail.com>
2019-06-10 18:05:43 +0700

    Test GIT
    Thêm báo cáo

---
+++ Libol/Libol/Controllers/BaoCaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Libol.Controllers
+{
+    public class BaoCaoController : Controller
+    {
+        // GET: BaoCao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
