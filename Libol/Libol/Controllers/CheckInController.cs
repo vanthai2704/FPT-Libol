10a4bd15cc8ee512cae04cd6395a4c028381ffde
YubelFTAE <32706401+YubelFTAE@users.noreply.github.com>
2019-06-12 04:07:07 +0700

    demo
    CheckIn, CheckOut book

---
+++ Libol/Libol/Controllers/CheckInController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Libol.Controllers
+{
+    public class CheckInController : Controller
+    {
+        // GET: CheckIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: CheckInSearch
+        public ActionResult CheckInSearch()
+        {
+            return View();
+        }
+        // GET: Giahan
+        public ActionResult Giahan()
+        {
+            return View();
+        }
+
+        // GET: CheckInSuccess
+        public ActionResult CheckInSuccess()
+        {
+            return View();
+        }
+    }
+}
