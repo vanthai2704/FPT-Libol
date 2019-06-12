1605f7dac9051ec7549373288541d756a55fad9e
trinhlvse05062 <dansondong1997@gmail.com>
2019-06-12 11:36:09 +0700

    TrinhLv1  commit UI
    Create UI for shelf

---
+++ Libol/Libol/Controllers/ShelfController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Libol.Controllers
+{
+    public class ShelfController : Controller
+    {
+        // GET: Shelf
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
