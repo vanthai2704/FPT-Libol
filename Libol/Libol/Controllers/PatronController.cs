dad8bbd394bf993e4a1689263b5ae77e77f80334
vanthai2704 <vanthai2704@gmail.com>
2019-06-16 21:06:35 +0700

    Change CIR_PATRON name, create 3 new stored procedure, create class CatalogueBusiness to handle business for catalogue
    FPT_SP_CATA_CHECK_EXIST_TITLE
    FPT_SP_CATA_GET_MARC_FORM
    FPT_SP_CATA_GETFIELDS_OF_FORM
    implement function GET_CATALOGUE_FIELDS in  CatalogueBusiness

--- Libol/Libol/Controllers/PatronController.cs
+++ Libol/Libol/Controllers/PatronController.cs
@@ -10,13 +10,15 @@ using Libol.Models;
 
 namespace Libol.Controllers
 {
-    public class CIR_PATRONController : Controller
+    public class PatronController : Controller
     {
         private LibolEntities db = new LibolEntities();
 
         // GET: CIR_PATRON
         public ActionResult Index()
         {
+           
+
             var cIR_PATRON = db.CIR_PATRON.Include(c => c.CIR_DIC_EDUCATION).Include(c => c.CIR_DIC_ETHNIC).Include(c => c.CIR_DIC_OCCUPATION).Include(c => c.CIR_PATRON_GROUP).Include(c => c.CIR_PATRON_UNIVERSITY);
             return View(cIR_PATRON.ToList());
         }
@@ -24,6 +26,7 @@ namespace Libol.Controllers
 
         public ActionResult PatronProfile()
         {
+           
             return View();
         }
 
@@ -61,12 +64,24 @@ namespace Libol.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ID,Code,ValidDate,ExpiredDate,LastIssuedDate,LastName,FirstName,MiddleName,Sex,DOB,EthnicID,EducationID,OccupationID,WorkPlace,Telephone,Mobile,Email,Portrait,PatronGroupID,Password,Status,Note,Debt,LastModifiedDate,InterestedSubjectBBK,InterestedSubjectDDC,IDCard")] CIR_PATRON cIR_PATRON)
         {
-            if (ModelState.IsValid)
-            {
-                db.CIR_PATRON.Add(cIR_PATRON);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+
+            var x = db.SP_CATA_GET_MARC_FORM(0, 0);
+   
+             var PatronGroups = db.SP_PAT_GET_PATRONGROUP().ToList();
+            var educations = db.SP_PAT_GET_EDUCATION().ToList();
+            var occupations = db.SP_PAT_GET_OCCUPATION().ToList();
+            var provinces = db.SP_PAT_GET_PROVINCE().ToList();
+            return RedirectToAction("Index");
+            //if (ModelState.IsValid)
+            //{
+            //    //db.CIR_PATRON.Add(cIR_PATRON);
+            //    var createDemo = db.SP_PAT_CREATE_PATRON("SE048933", "6/2/2019", "6/11/2019", "6/1/2019", "Thái", "Nguyễn", "Văn", true, "6/11/1997", 1, 31, 50, "Công ty FPT software", "0969347967", "", "thainvse04893@fpt.edu.vn", "", 1, "Test", 0, "", new System.Data.Entity.Core.Objects.ObjectParameter("intRetval", ParameterDirection.Output));
+                
+            //    db.SaveChanges();
+            //    return RedirectToAction("Index");
+            //    //db.SaveChanges();
+            //    //return RedirectToAction("Index");
+            //}
 
             ViewBag.EducationID = new SelectList(db.CIR_DIC_EDUCATION, "ID", "EducationLevel", cIR_PATRON.EducationID);
             ViewBag.EthnicID = new SelectList(db.CIR_DIC_ETHNIC, "ID", "Ethnic", cIR_PATRON.EthnicID);
@@ -152,11 +167,6 @@ namespace Libol.Controllers
             }
             base.Dispose(disposing);
         }
-
-
-
-
-
-
+      
     }
 }
---
+++ Libol/Libol/Models/CatalogueBusiness.cs
@@ -0,0 +1,31 @@
+﻿using Libol.EntityResult;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Libol.Models
+{
+
+    public class CatalogueBusiness
+    {
+        LibolEntities db = new LibolEntities();
+
+        public CatalogueBusiness()
+        {
+
+        }
+
+        // Modified from store procedure SP_CATA_GET_CATALOGUE_FIELDS
+        // data demo GET_CATALOGUE_FIELDS(0, 12, "100$e,010$a,010$b", "", 1);
+        public List<GET_CATALOGUE_FIELDS_Result> GET_CATALOGUE_FIELDS(int intIsAuthority, int intFormID, string strFieldCodes, string strAddedFieldCodes, int intGroupBy)
+        {
+            List<GET_CATALOGUE_FIELDS_Result> list = db.Database.SqlQuery<GET_CATALOGUE_FIELDS_Result>("SP_CATA_GET_CATALOGUE_FIELDS {0}, {1}, {2},{3},{4}",
+                new object[] { intIsAuthority, intFormID, strFieldCodes, strAddedFieldCodes, 1 }).ToList();
+            return list;
+        }
+
+    }
+}
