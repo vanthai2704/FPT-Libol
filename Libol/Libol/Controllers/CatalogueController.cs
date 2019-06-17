b7ed2176ffb43a9290e83218a895f872b88d6e04
Scorpion1406 <31349402+Scorpion1406@users.noreply.github.com>
2019-06-17 10:40:22 +0700

    Catalogue Update
    Catalogue Update

--- Libol/Libol/Controllers/CatalogueController.cs
+++ Libol/Libol/Controllers/CatalogueController.cs
@@ -3,35 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Libol.Models;
 
 namespace Libol.Controllers
 {
     public class CatalogueController : Controller
     {
+        private LibolEntities db = new LibolEntities();
+
         // GET: Catalogue
         public ActionResult MainTab()
         {
+            
             return View();
         }
 
+ 
+
+        //----------------Add New Cata ----------------
+        //---------------------------------------------
         public ActionResult AddNewCatalogue()
         {
+            ViewData["ListMarcForm"] = ListMarcForm();
             return View();
         }
 
-        public ActionResult SearchView()
+        public List<FPT_SP_CATA_GET_MARC_FORM_Result> ListMarcForm()
         {
-            return View();
+            List<FPT_SP_CATA_GET_MARC_FORM_Result> list = db.FPT_SP_CATA_GET_MARC_FORM(0, 0).ToList();
+            return list;
+
+        }
+
+        public String GetFieldByID(int SelectedIndex)
+        {
+            List<FPT_SP_CATA_GETFIELDS_OF_FORM_Result> GetForm = db.FPT_SP_CATA_GETFIELDS_OF_FORM(SelectedIndex , "" , null).ToList();
+
         }
 
 
+
+        //----------------Search Field Cata -----------
+        //---------------------------------------------
         public ActionResult SearchCodeNumber()
         {
             return View();
         }
-        
 
-            public ActionResult UpdateCatalogue()
+        //----------------Update Cata -----------
+        //---------------------------------------------
+        public ActionResult UpdateCatalogue()
         {
             return View();
         }
