f1f4faa950758417f3f9c3190ace9c3a364c1afa
ngquangdat <ngquangdat13@gmail.com>
2019-06-17 23:10:02 +0700

    Fix bug expire session redirect to Login + update jquery

--- Libol/Libol/Controllers/BaseController.cs
+++ Libol/Libol/Controllers/BaseController.cs
@@ -14,6 +14,11 @@ namespace Libol.Controllers
             var session = Session["UserID"];
             if(session == null)
             {
+                if (filterContext.HttpContext.Request.IsAjaxRequest())
+                {
+                    filterContext.HttpContext.Response.StatusCode = 401;
+                    filterContext.HttpContext.Response.End();
+                }
                 filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Login", action = "Index" }));
             }
             base.OnActionExecuting(filterContext);
