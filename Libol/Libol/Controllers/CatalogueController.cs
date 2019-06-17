117f92b96fcfe78be0522b39c27fd4e43c57e8b0
Scorpion1406 <31349402+Scorpion1406@users.noreply.github.com>
2019-06-17 16:28:30 +0700

    Catalogue Update

--- Libol/Libol/Controllers/CatalogueController.cs
+++ Libol/Libol/Controllers/CatalogueController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Libol.Models;
+using Libol.EntityResult;
 
 namespace Libol.Controllers
 {
@@ -27,7 +28,16 @@ namespace Libol.Controllers
             ViewData["ListMarcForm"] = ListMarcForm();
             return View();
         }
+        [HttpPost]
+        public JsonResult LoadFormComplated(int intIsAuthority, int intFormID)
+        {
+            string fieldCode = GetFieldByID(intIsAuthority,"", intFormID);
+            List<GET_CATALOGUE_FIELDS_Result> formComplated = Catalogue(intIsAuthority, intFormID, fieldCode);
+            ViewData["MarcFormComplated"] = formComplated;
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
 
+        //get list marc form
         public List<FPT_SP_CATA_GET_MARC_FORM_Result> ListMarcForm()
         {
             List<FPT_SP_CATA_GET_MARC_FORM_Result> list = db.FPT_SP_CATA_GET_MARC_FORM(0, 0).ToList();
@@ -35,11 +45,26 @@ namespace Libol.Controllers
 
         }
 
-        public String GetFieldByID(int SelectedIndex)
+        
+        //get all fields by ID 
+        public String GetFieldByID(int intIsAuthority ,string strCreator, int SelectedIndex)
         {
-            List<FPT_SP_CATA_GETFIELDS_OF_FORM_Result> GetForm = db.FPT_SP_CATA_GETFIELDS_OF_FORM(SelectedIndex , "" , null).ToList();
-
+            List<FPT_SP_CATA_GETFIELDS_OF_FORM_Result> GetForm = db.FPT_SP_CATA_GETFIELDS_OF_FORM(SelectedIndex, "", 0).ToList();
+            string fields = "";
+            foreach(FPT_SP_CATA_GETFIELDS_OF_FORM_Result item in GetForm)
+            {
+                fields = fields + item.FieldCode + ",";
+            }
+            return fields;
         }
+
+        //Get Catalogue
+        public List<GET_CATALOGUE_FIELDS_Result> Catalogue(int intIsAuthority, int intFormID, string strFieldCodes)
+        {
+            List<GET_CATALOGUE_FIELDS_Result> list = db.FPT_GET_CATALOGUE_FIELDS(intIsAuthority , intFormID, strFieldCodes, "", 0);
+            return list;
+        }
+        
 
 
 
