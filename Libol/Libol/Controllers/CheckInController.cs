87dc20ecfa27be5e807194da1b0299d9c8ea5051
YubelFTAE <32706401+YubelFTAE@users.noreply.github.com>
2019-06-21 01:34:48 +0700

    Checkin showpatroninfo

--- Libol/Libol/Controllers/CheckInController.cs
+++ Libol/Libol/Controllers/CheckInController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Libol.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,23 @@ namespace Libol.Controllers
 {
     public class CheckInController : BaseController
     {
+        private LibolEntities db = new LibolEntities();
+
         // GET: CheckIn
         public ActionResult Index()
         {
             return View();
         }
 
-        [HttpGet]
-        public PartialViewResult CheckInByCardNumber()
+        [HttpPost]
+        public PartialViewResult CheckInByCardNumber(string strFullName, string strPatronCode, string strFixDueDate)
         {
+            SP_GET_PATRON_INFOR_Result patroninfo =
+                db.SP_GET_PATRON_INFOR(strFullName, strPatronCode, strFixDueDate).First();
+            ViewData["patroninfo"] = patroninfo;
+
+            List<SP_GET_PATRON_ONLOAN_COPIES_Result> patronloaninfo = db.SP_GET_PATRON_ONLOAN_COPIES(patroninfo.ID).ToList<SP_GET_PATRON_ONLOAN_COPIES_Result>();
+            ViewData["patronloaninfo"] = patronloaninfo;
             return PartialView("_checkinByCardNumber");
         }
 
