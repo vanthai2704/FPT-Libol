ec9cc6b03b36cdc009e2644ab1b9a0f4c3e6cb68
vanthai2704 <vanthai2704@gmail.com>
2019-06-26 10:53:11 +0700

    Load Libraries and Location

---
+++ Libol/Libol/EntityResult/SP_HOLDING_LIBRARY_SELECT_Result.cs
@@ -0,0 +1,25 @@
+﻿using Libol.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libol.EntityResult
+{
+    public class SP_HOLDING_LIBRARY_SELECT_Result
+    {
+        public SP_HOLDING_LIBRARY_SELECT_Result()
+        {
+
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public bool LocalLib { get; set; }
+        public string Address { get; set; }
+        public string AccessEntry { get; set; }
+        public string FullName { get; set; }
+
+    }
+}
