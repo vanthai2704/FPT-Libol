e03e2578357c7de82e81c34a125fdb1516103e6a
YubelFTAE <32706401+YubelFTAE@users.noreply.github.com>
2019-07-12 14:08:54 +0700

    update gia han

--- Libol/Libol/Controllers/RenewController.cs
+++ Libol/Libol/Controllers/RenewController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Libol.EntityResult;
+using Libol.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,20 @@ namespace Libol.Controllers
 {
     public class RenewController : Controller
     {
+        private LibolEntities db = new LibolEntities();
+        RenewBusiness renewBusiness = new RenewBusiness();
+
         // GET: Renew
         public ActionResult Renew()
         {
+            ViewBag.ContentRenew = new List<SP_CIR_GET_RENEW_Result>();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SearchToRenew(int intUserID, Byte intType, string strCodeVal)
+        {
+            ViewBag.ContentRenew = renewBusiness.SP_CIR_GET_RENEW(intUserID, intType, strCodeVal);
             return View();
         }
     }
