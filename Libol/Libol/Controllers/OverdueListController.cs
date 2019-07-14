dad623bb679c9a1a663fe04925de689967e4cb90
YubelFTAE <32706401+YubelFTAE@users.noreply.github.com>
2019-07-14 08:47:48 +0700

    add quá hạn

---
+++ Libol/Libol/Controllers/OverdueListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Libol.Controllers
+{
+    public class OverdueListController : Controller
+    {
+        // GET: OverdueList
+        public ActionResult OverdueList()
+        {
+            return View();
+        }
+    }
+}
