ab2d396549209511ff6e835137ac1466f4d7ffb5
YubelFTAE <32706401+YubelFTAE@users.noreply.github.com>
2019-07-15 15:47:03 +0700

    update quá hạn

--- Libol/Libol/Controllers/OverdueListController.cs
+++ Libol/Libol/Controllers/OverdueListController.cs
@@ -14,8 +14,16 @@ namespace Libol.Controllers
         // GET: OverdueList
         public ActionResult OverdueList()
         {
-            ViewBag.listOverdue = GET_LIST_OVERDUELIST_GETINFOR(43, "", "").ToList();
+            //ViewBag.listOverdue = GET_LIST_OVERDUELIST_GETINFOR(43, "", "").ToList();
             return View();
+        }
+
+        [HttpPost]
+        public PartialViewResult OverdueListResult(Nullable<int> intUserID, string strPatronIDs,string txtSoThe, string txtTenBanDoc, int ddlNhomBanDoc, int ddlTruong, int ddlKhoa, string txtKhoaHoc, string txtLopHoc, int ddlLib, int ddlLoc, string txtTenTaiLieu, string txtSDKCB, DateTime? txtNgayMuonTu, DateTime? txtNgayMuonDen, DateTime? txtNgayTraTu, DateTime? txtNgayTraDen, string txtSoNgayQuaHan, string txtSoNgayQuaHanDen)
+        {
+            string whereCondition = ProcessCondition( txtSoThe,  txtTenBanDoc,  ddlNhomBanDoc,  ddlTruong,  ddlKhoa,  txtKhoaHoc,  txtLopHoc,  ddlLib,  ddlLoc,  txtTenTaiLieu,  txtSDKCB,  txtNgayMuonTu,  txtNgayMuonDen,  txtNgayTraTu,  txtNgayTraDen,  txtSoNgayQuaHan,  txtSoNgayQuaHanDen);
+            ViewBag.listOverdue = GET_LIST_OVERDUELIST_GETINFOR(intUserID, "", whereCondition).ToList();
+            return PartialView("_OverdueListResult");
         }
 
         // trinhlv1
@@ -26,44 +34,44 @@ namespace Libol.Controllers
             return list;
         }
 
-        //private string ProcessCondition(string txtSoThe,string txtTenBanDoc,string txtKhoaHoc,string txtLopHoc,string txtTenTaiLieu, string txtSDKCB,DateTime txtNgayMuonTu, DateTime txtNgayMuonDen,DateTime txtNgayTraTu, DateTime txtNgayTraDen,int txtSoNgayQuaHan,int txtSoNgayQuaHanDen)
-        //{
-        //    string str = "";
-        //    if (String.Compare(txtSoThe.Trim(), "", false) != 0)
-        //        str = str + " AND UPPER(A.PatronCode) = '" + txtSoThe.Trim().ToUpper() + "'";
-        //    if (String.Compare(txtTenBanDoc.Trim(), "", false) != 0)
-        //        str = str + " AND UPPER(A.Name) LIKE N'%" + txtTenBanDoc.Trim().ToUpper() + "%'";
-        //    if (this.ddlNhomBanDoc.SelectedIndex > 0)
-        //        str = str + " AND A.PatronGroupID = " + StringType.FromInteger(Convert.ToInt32(this.ddlNhomBanDoc.SelectedValue));
-        //    if (this.ddlTruong.SelectedIndex > 0)
-        //        str = str + " AND A.CollegeID = " + StringType.FromInteger(Convert.ToInt32(this.ddlTruong.SelectedValue));
-        //    if (this.ddlKhoa.SelectedIndex > 0)
-        //        str = str + " AND A.FacultyID = " + StringType.FromInteger(Convert.ToInt32(this.ddlKhoa.SelectedValue));
-        //    if (String.Compare(txtKhoaHoc.Trim(), "", false) != 0)
-        //        str = str + " AND UPPER(A.Grade) LIKE N'%" + txtKhoaHoc.Trim().ToUpper() + "%'";
-        //    if (String.Compare(txtLopHoc.Trim(), "", false) != 0)
-        //        str = str + " AND UPPER(A.Class) LIKE N'%" + txtLopHoc.Trim().ToUpper() + "%'";
-        //    if (this.ddlLib.SelectedIndex > 0)
-        //        str = str + " AND A.LibID = " + StringType.FromInteger(Convert.ToInt32(this.ddlLib.SelectedValue));
-        //    if (this.ddlLoc.SelectedIndex > 0)
-        //        str = str + " AND A.LocID = " + StringType.FromInteger(Convert.ToInt32(this.ddlLoc.SelectedValue));
-        //    if (String.Compare(txtTenTaiLieu.Trim(), "", false) != 0)
-        //        str = str + " AND UPPER(A.MainTitle) LIKE N'%" + txtTenTaiLieu.Trim().ToUpper() + "%'";
-        //    if (String.Compare(txtSDKCB.Trim(), "", false) != 0)
-        //        str = str + " AND UPPER(A.CopyNumber) LIKE '%" + txtSDKCB.Trim().ToUpper() + "%'";
-        //    if (String.Compare(txtNgayMuonTu.ToString("yyyyMMdd"), "", false) != 0)
-        //        str = str + " AND CONVERT(VARCHAR(10), A.CheckOutDate, 112) >= " + txtNgayMuonTu.ToString("yyyyMMdd");
-        //    if (String.Compare(txtNgayMuonDen.ToString("yyyyMMdd"), "", false) != 0)
-        //        str = str + " AND CONVERT(VARCHAR(10), A.CheckOutDate, 112) <= " + txtNgayMuonDen.ToString("yyyyMMdd");
-        //    if (String.Compare(txtNgayTraTu.ToString("yyyyMMdd"), "", false) != 0)
-        //        str = str + " AND CONVERT(VARCHAR(10), A.CheckInDate, 112) >= " + txtNgayTraTu.ToString("yyyyMMdd");
-        //    if (String.Compare(txtNgayTraDen.ToString("yyyyMMdd"), "", false) != 0)
-        //        str = str + " AND CONVERT(VARCHAR(10), A.CheckInDate, 112) <= " + txtNgayTraDen.ToString("yyyyMMdd");
-        //    if (String.Compare(txtSoNgayQuaHan.ToString(), "", false) != 0)
-        //        str = str + " AND A.OverdueDate >= " +txtSoNgayQuaHan;
-        //    if (String.Compare(txtSoNgayQuaHanDen.ToString(), "", false) != 0)
-        //        str = str + " AND A.OverdueDate <= " + txtSoNgayQuaHanDen;
-        //    return str;
-        //}
+        private string ProcessCondition(string txtSoThe, string txtTenBanDoc,int ddlNhomBanDoc,int ddlTruong,int ddlKhoa, string txtKhoaHoc, string txtLopHoc,int ddlLib,int ddlLoc, string txtTenTaiLieu, string txtSDKCB, DateTime? txtNgayMuonTu, DateTime? txtNgayMuonDen, DateTime? txtNgayTraTu, DateTime? txtNgayTraDen, string txtSoNgayQuaHan, string txtSoNgayQuaHanDen)
+        {
+            string str = "";
+            if (String.Compare(txtSoThe.Trim(), "", false) != 0)
+                str = str + " AND UPPER(A.PatronCode) = '" + txtSoThe.Trim().ToUpper() + "'";
+            if (String.Compare(txtTenBanDoc.Trim(), "", false) != 0)
+                str = str + " AND UPPER(A.Name) LIKE N'%" + txtTenBanDoc.Trim().ToUpper() + "%'";
+            if (ddlNhomBanDoc != -1)
+                str = str + " AND A.PatronGroupID = " + ddlNhomBanDoc;
+            if (ddlTruong != -1)
+                str = str + " AND A.CollegeID = " + ddlTruong;
+            if (ddlKhoa != -1)
+                str = str + " AND A.FacultyID = " + ddlKhoa;
+            if (String.Compare(txtKhoaHoc.Trim(), "", false) != 0)
+                str = str + " AND UPPER(A.Grade) LIKE N'%" + txtKhoaHoc.Trim().ToUpper() + "%'";
+            if (String.Compare(txtLopHoc.Trim(), "", false) != 0)
+                str = str + " AND UPPER(A.Class) LIKE N'%" + txtLopHoc.Trim().ToUpper() + "%'";
+            if (ddlLib != -1)
+                str = str + " AND A.LibID = " + ddlLib;
+            if (ddlLoc != -1)
+                str = str + " AND A.LocID = " + ddlLoc;
+            if (String.Compare(txtTenTaiLieu.Trim(), "", false) != 0)
+                str = str + " AND UPPER(A.MainTitle) LIKE N'%" + txtTenTaiLieu.Trim().ToUpper() + "%'";
+            if (String.Compare(txtSDKCB.Trim(), "", false) != 0)
+                str = str + " AND UPPER(A.CopyNumber) LIKE '%" + txtSDKCB.Trim().ToUpper() + "%'";
+            if (!Equals(txtNgayMuonTu, null))
+                str = str + " AND CONVERT(VARCHAR(10), A.CheckOutDate, 112) >= " + txtNgayMuonTu.Value.ToString("yyyyMMdd");
+            if (!Equals(txtNgayMuonDen,null))
+                str = str + " AND CONVERT(VARCHAR(10), A.CheckOutDate, 112) <= " + txtNgayMuonDen.Value.ToString("yyyyMMdd");
+            if (!Equals(txtNgayTraTu, null))
+                str = str + " AND CONVERT(VARCHAR(10), A.CheckInDate, 112) >= " + txtNgayTraTu.Value.ToString("yyyyMMdd");
+            if (!Equals(txtNgayTraDen, null))
+                str = str + " AND CONVERT(VARCHAR(10), A.CheckInDate, 112) <= " + txtNgayTraDen.Value.ToString("yyyyMMdd");
+            if (String.Compare(txtSoNgayQuaHan, "", false) != 0)
+                str = str + " AND A.OverdueDate >= " + txtSoNgayQuaHan;
+            if (String.Compare(txtSoNgayQuaHanDen, "", false) != 0)
+                str = str + " AND A.OverdueDate <= " + txtSoNgayQuaHanDen;
+            return str;
+        }
     }
 }
