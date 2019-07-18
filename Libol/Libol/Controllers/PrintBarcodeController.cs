489e8283c953cb561683e8673f235b58db38b467
ngquangdat <ngquangdat13@gmail.com>
2019-07-18 16:02:52 +0700

    Update Print function

--- Libol/Libol/Controllers/PrintBarcodeController.cs
+++ Libol/Libol/Controllers/PrintBarcodeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Libol.EntityResult;
+using Libol.Models;
+using Libol.SupportClass;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,25 @@ namespace Libol.Controllers
 {
     public class PrintBarcodeController : Controller
     {
-        // GET: PrintBarcode
+        private LibolEntities db = new LibolEntities();
+        ShelfBusiness shelfBusiness = new ShelfBusiness();
+
+        [AuthAttribute(ModuleID = 4, RightID = "103")]
         public ActionResult Index()
         {
+            ViewBag.Library = shelfBusiness.FPT_SP_HOLDING_LIBRARY_SELECT(0, 1, -1, (int)Session["UserID"], 1);
+            ViewBag.Template = db.SP_SYS_GET_TEMPLATE(0, 79).ToList();
             return View();
         }
+
+        [HttpPost]
+        [AuthAttribute(ModuleID = 4, RightID = "103")]
+        public JsonResult OnchangeLibrary(int LibID)
+        {
+            List<SP_HOLDING_LOCATION_GET_INFO_Result> list = shelfBusiness.FPT_SP_HOLDING_LOCATION_GET_INFO(LibID, (int)Session["UserID"], 0, -1);
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        
     }
 }
