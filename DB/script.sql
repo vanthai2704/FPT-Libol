3c0496024bdce5b457897dac71d3fda49bf21ce0
ngquangdat <ngquangdat13@gmail.com>
2019-07-23 10:10:26 +0700

    Update script

--- DB/script.sql
+++ DB/script.sql
@@ -40,36 +40,6 @@ CREATE TABLE SYS_USER_GOOGLE_ACCOUNT(
 	ID INT NOT NULL
 )
 
-/****** Object:  Table [dbo].[FPT_RECOMMEND]    Script Date: 07/07/2019 19:01:47 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE [dbo].[FPT_RECOMMEND](
-	[POID] [int] NOT NULL,
-	[ReID] [varchar](30) NOT NULL,
- CONSTRAINT [PK_FPT_RECOMMEND] PRIMARY KEY CLUSTERED 
-(
-	[POID] ASC,
-	[ReID] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
-
-SET ANSI_PADDING OFF
-GO
-
-ALTER TABLE [dbo].[FPT_RECOMMEND]  WITH CHECK ADD  CONSTRAINT [FK_FPT_RECOMMEND_ACQ_PO] FOREIGN KEY([POID])
-REFERENCES [dbo].[ACQ_PO] ([ID])
-GO
-
-ALTER TABLE [dbo].[FPT_RECOMMEND] CHECK CONSTRAINT [FK_FPT_RECOMMEND_ACQ_PO]
 GO
 
 /******/
@@ -2994,58 +2964,6 @@ NOT IN (
 )
 
 
-GO
-/****** Object:  StoredProcedure [dbo].[FPT_GET_PATRON_LOCK_STATISTIC]    Script Date: 07/15/2019 09:39:04 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE PROCEDURE [dbo].[FPT_GET_PATRON_LOCK_STATISTIC]
--- Purpose: Get patron lock statistic
--- Created AnNXT
--- Date 14/07/2019
--- ModifyDate:
-	@strPatronCode  varchar(50),
-	--@strNote nvarchar(200),
-	@strLockDateFrom varchar(30),
-	@strLockDateTo varchar(30),
-	@intCollegeID int
-AS
-	DECLARE @strSql varchar(1000)
-	DECLARE @strJoinSQL varchar(1000)
-	DECLARE @strLikeSql varchar(1000)
-	
-	SET @strSql = 'SELECT CPL.PatronCode, CPL.StartedDate, CPL.Note, CP.FirstName + '' '' + CP.MiddleName + '' '' + CP.LastName as FullName, CPL.StartedDate + CPL.LockedDays as FinishDate, CPL.LockedDays '
-	SET @strLikeSql = '1 =1 AND '
-	SET @strJoinSQL = ''
-
-	SET @strJoinSQL = @strJoinSQL + ' FROM CIR_PATRON_LOCK CPL, CIR_PATRON CP'
-	SET @strLikeSql = @strLikeSql + ' UPPER(CPL.PatronCode)=UPPER(CP.Code) AND'
-	
-	IF NOT @strPatronCode = ''
-		SET @strLikeSql = @strLikeSql + ' UPPER(CPL.PatronCode)=''' + UPPER(@strPatronCode) + ''' AND'
-	
-	IF NOT @intCollegeID = 0
-		BEGIN
-			SET @strSql = @strSql + ', CDC.COLLEGE'
-			SET @strJoinSQL = @strJoinSQL + ', CIR_PATRON_UNIVERSITY CPU, CIR_DIC_COLLEGE CDC'
-			SET @strLikeSql = @strLikeSql + ' CP.ID = CPU.PATRONID AND CPU.COLLEGEID = CDC.ID AND CPU.COLLEGEID = ' + CAST(@intCollegeID AS VARCHAR(20)) + ' AND'
-		END
-	
-	--IF NOT @strNote = ''	
-	--	BEGIN
-	--		SET @strNote = UPPER(@strNote)
-	--		SET @strLikeSQL = @strLikeSQL + ' UPPER(CPL.Note)=N'''+ @strNote +''' AND'
-	--	END
-	
-	IF NOT @strLockDateFrom = ''
-		SET @strLikeSql = @strLikeSql + ' CPL.StartedDate >= ''' + @strLockDateFrom + ''' AND'
-	IF NOT @strLockDateTo = ''
-		SET @strLikeSql = @strLikeSql + ' CPL.StartedDate <= ''' + @strLockDateTo + ''' AND'
-	
-	SET @strSql = @strSql + @strJoinSQL + ' WHERE ' +@strLikeSQL
-	SET @strSql = LEFT(@strSql,LEN(@strSql)-3) + ' ORDER BY CPL.StartedDate DESC'
-EXEC (@stRSql)
 
 
 GO
