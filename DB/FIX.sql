9276f26eda54971d543464573c0a9b87cbb108bf
ngquangdat <ngquangdat13@gmail.com>
2019-07-29 11:28:42 +0700

    Create FIX.sql

---
+++ DB/FIX.sql
@@ -0,0 +1,52 @@
+GO
+/****** Object:  StoredProcedure [dbo].[FPT_GET_PATRON_LOCK_STATISTIC]    Script Date: 07/27/2019 14:34:23 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[FPT_GET_PATRON_LOCK_STATISTIC]
+-- Purpose: Get patron lock statistic
+-- Created AnNXT
+-- Date 14/07/2019
+-- ModifyDate:
+	@strPatronCode  varchar(50),
+	@strNote nvarchar(200),
+	@strLockDateFrom varchar(30),
+	@strLockDateTo varchar(30),
+	@intCollegeID int
+AS
+	DECLARE @strSql nvarchar(1000)
+	DECLARE @strJoinSQL nvarchar(1000)
+	DECLARE @strLikeSql nvarchar(1000)
+	
+	SET @strSql = 'SELECT CPL.PatronCode, CPL.StartedDate, CPL.Note, ISNULL(CP.FirstName,'''') + '' '' + ISNULL(CP.MiddleName,'''') + '' '' + ISNULL(CP.LastName,'''') as FullName, CPL.StartedDate + CPL.LockedDays as FinishDate, CPL.LockedDays '
+	SET @strLikeSql = '1 =1 AND '
+	SET @strJoinSQL = ''
+
+	SET @strJoinSQL = @strJoinSQL + ' FROM CIR_PATRON_LOCK CPL, CIR_PATRON CP'
+	SET @strLikeSql = @strLikeSql + ' UPPER(CPL.PatronCode)=UPPER(CP.Code) AND'
+	
+	IF NOT @strPatronCode = ''
+		SET @strLikeSql = @strLikeSql + ' UPPER(CPL.PatronCode)=''' + UPPER(@strPatronCode) + ''' AND'
+	
+	IF NOT @intCollegeID = 0
+		BEGIN
+			SET @strSql = @strSql + ', CDC.COLLEGE'
+			SET @strJoinSQL = @strJoinSQL + ', CIR_PATRON_UNIVERSITY CPU, CIR_DIC_COLLEGE CDC'
+			SET @strLikeSql = @strLikeSql + ' CP.ID = CPU.PATRONID AND CPU.COLLEGEID = CDC.ID AND CPU.COLLEGEID = ' + CAST(@intCollegeID AS VARCHAR(20)) + ' AND'
+		END
+	
+	IF NOT @strNote = ''	
+		BEGIN
+			SET @strLikeSQL = @strLikeSQL + ' CPL.Note LIKE N''%' + @strNote + '%'' AND'
+		END
+	
+	IF NOT @strLockDateFrom = ''
+		SET @strLikeSql = @strLikeSql + ' CPL.StartedDate >= ''' + @strLockDateFrom + ''' AND'
+		
+	IF NOT @strLockDateTo = ''
+		SET @strLikeSql = @strLikeSql + ' CPL.StartedDate <= ''' + @strLockDateTo + ''' AND'
+	
+	SET @strSql = @strSql + @strJoinSQL + ' WHERE ' +@strLikeSQL
+	SET @strSql = LEFT(@strSql,LEN(@strSql)-3) + ' ORDER BY CPL.StartedDate DESC'
+EXEC (@stRSql)
