9b0a00dee4d33aa4ded66e235d98173e0350ed74
Scorpion1406 <31349402+Scorpion1406@users.noreply.github.com>
2019-08-01 23:31:44 +0700

    Doanhdq 1/8

--- Libol/Libol/SupportClass/FormatHoldingTitle.cs
+++ Libol/Libol/SupportClass/FormatHoldingTitle.cs
@@ -7,19 +7,31 @@ namespace Libol.SupportClass
 {
     public class FormatHoldingTitle
     {
-        public string OnFormatHoldingTitle(string title)
+        public string OnFormatHoldingTitle(string value)
         {
-            string validate = title.Replace("$a", "");
-            validate = validate.Replace("$b", "");
-            validate = validate.Replace("$c", "");
-            validate = validate.Replace("=$b", "");
-            validate = validate.Replace(":$b", "");
-            validate = validate.Replace("/$c", "");
-            validate = validate.Replace(".$n", "");
-            validate = validate.Replace(":$p", "");
-            validate = validate.Replace(";$c", "");
-            validate = validate.Replace("+$e", "");
-            return validate;
+
+            int index = 0;
+            //Loc ki tu $x 
+            while (true)
+            {
+                index = value.IndexOf("$");
+                if (index == -1) break;
+                value = value.Substring(0, index) + value.Substring(index + 2);
+            }
+
+            return value;
+
+            //string validate = title.Replace("$a", "");
+            //validate = validate.Replace("$b", "");
+            //validate = validate.Replace("$c", "");
+            //validate = validate.Replace("=$b", "");
+            //validate = validate.Replace(":$b", "");
+            //validate = validate.Replace("/$c", "");
+            //validate = validate.Replace(".$n", "");
+            //validate = validate.Replace(":$p", "");
+            //validate = validate.Replace(";$c", "");
+            //validate = validate.Replace("+$e", "");
+            //return validate;
         }
     }
 }
