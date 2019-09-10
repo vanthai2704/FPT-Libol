14de225716c0ec56c3d983aea3adedb93bb5cb2d
YubelFTAE <32706401+YubelFTAE@users.noreply.github.com>
2019-09-10 10:45:31 +0700

    Send mail to nguyennm2

--- Libol/Libol/Startup.cs
+++ Libol/Libol/Startup.cs
@@ -14,11 +14,13 @@ namespace Libol
     {
         public void Configuration(IAppBuilder app)
         {
-            //GlobalConfiguration.Configuration.UseSqlServerStorage(@"Data Source=flibserver.database.windows.net;Initial Catalog=flib;User ID=flibadmin;Password=Chipchip11;");
-            //app.UseHangfireDashboard();
-            //OverdueEmailNoticeController obj = new OverdueEmailNoticeController();
-            //RecurringJob.AddOrUpdate(() => obj.SendEmail("hongnhat97tt@gmail.com"), Cron.Daily);
-            //app.UseHangfireServer();
+            GlobalConfiguration.Configuration.UseSqlServerStorage(@"Data Source=flibserver.database.windows.net;Initial Catalog=flib;User ID=flibadmin;Password=Chipchip11;");
+            //GlobalConfiguration.Configuration.UseSqlServerStorage(@"data source =VAN_SANG\SQLEXPRESS ; initial catalog = Libol; integrated security = True;");
+            app.UseHangfireDashboard();
+            OverdueEmailNoticeController obj = new OverdueEmailNoticeController();
+            RecurringJob.AddOrUpdate(() => obj.SendEmail("nguyennm2@fe.edu.vn"), Cron.Daily);
+            //RecurringJob.AddOrUpdate(() => obj.SendEmail("hongnhat97tt@gmail.com"), Cron.Hourly);
+            app.UseHangfireServer();
         }
     }
 }
