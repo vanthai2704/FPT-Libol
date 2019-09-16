ccaf97cc3ad330d11eaf6b0a6be89b1fbf36ca0a
Nguyen Van Thai <vanthai2704@gmail.com>
2019-09-16 14:49:39 +0700

    commit bao cao

--- Libol/Libol/Models/Libol.Designer.cs
+++ Libol/Libol/Models/Libol.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\gitmoi\FPT-Libol\Libol\Libol\Models\Libol.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\FLib\FPT-Libol\Libol\Libol\Models\Libol.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
